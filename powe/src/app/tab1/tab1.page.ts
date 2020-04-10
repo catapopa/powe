2ca39c50488238a9274fbe300a080926da0af9d7
Cata Popa <cataqoq@gmail.com>
2020-04-10 16:50:08 +0300

    Login button, OAuth2.

--- powe/src/app/tab1/tab1.page.ts
+++ powe/src/app/tab1/tab1.page.ts
@@ -1,22 +1,51 @@
-import { Component } from '@angular/core';
-import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Component, OnInit } from '@angular/core';
+import { registerWebPlugin } from '@capacitor/core';
+import { OAuth2Client } from '@byteowls/capacitor-oauth2';
+import { environment } from 'src/environments/environment';
+
+import { Plugins } from '@capacitor/core';
+
 
 @Component({
   selector: 'powe-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor(private googlePlus: GooglePlus) { }
-
-  getData() {
-    this.googlePlus.login({
-      webClientId: '1068508048504-gg5q0s8e1urtvuh26ggalf69n54mhi5l.apps.googleusercontent.com',
-      offline: true
-    })
-      .then(res => console.log(res))
-      .catch(err => console.error(err));
+  ngOnInit() {
+    console.log('Register custom capacitor plugins');
+    registerWebPlugin(OAuth2Client);
+    // other stuff
   }
 
+  constructor() { }
+
+  googleLogin() {
+    Plugins.OAuth2Client.authenticate({
+      appId: environment.web.client_id,
+      authorizationBaseUrl: environment.web.auth_uri,
+      accessTokenEndpoint: environment.web.accessTokenEndpoint,
+      scope: environment.web.scope,
+      resourceUrl: environment.web.resourceUrl,
+      web: {
+        redirectUrl: environment.web.javascript_origins[0],
+        windowOptions: 'height=600,left=0,top=0'
+      },
+      // android: {
+      //   appId: environment.oauthAppId.google.android,
+      //   responseType: 'code', // if you configured a android app in google dev console the value must be "code"
+      //   customScheme: 'com.companyname.appname:/' // package name from google dev console
+      // },
+      // ios: {
+      //   appId: environment.oauthAppId.google.ios,
+      //   responseType: 'code', // if you configured a ios app in google dev console the value must be "code"
+      //   customScheme: 'com.companyname.appname:/' // Bundle ID from google dev console
+      // }
+    }).then(resourceUrlResponse => {
+      console.log(resourceUrlResponse);
+    }).catch(reason => {
+      console.error('Google OAuth rejected', reason);
+    });
+  }
 }
--- powe/src/environments/environment.ts
+++ powe/src/environments/environment.ts
@@ -2,14 +2,17 @@ export const environment = {
   production: false,
   // serviceHost: 'https://localhost:44303/api/',
   whitelistedDomains: ['localhost:8100'],
-  client_id: '1068508048504-gg5q0s8e1urtvuh26ggalf69n54mhi5l.apps.googleusercontent.com',
   web:
   {
     client_id: '1068508048504-gg5q0s8e1urtvuh26ggalf69n54mhi5l.apps.googleusercontent.com',
-    project_id: 'powe-273408', auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    project_id: 'powe-273408',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
     token_uri: 'https://oauth2.googleapis.com/token',
     auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     client_secret: 'Wgl5YS1hPb1IyjBwb_3hYTMM',
-    javascript_origins: ['http://localhost:8100']
+    javascript_origins: ['http://localhost:8100'],
+    scope: 'email profile',
+    accessTokenEndpoint: 'https://www.googleapis.com/oauth2/v4/token',
+    resourceUrl: 'https://www.googleapis.com/userinfo/v2/me',
   }
 };
